ef6e16fa09311fdb6ac719e4dd6b6031f7012c2f
saraems <sara.ems.94@gmail.com>
2021-12-05 15:09:12 +0100

    Only print

--- test.py
+++ test.py
@@ -25,21 +25,21 @@ ser = serial.Serial(
 while 1:
 
 	print(ser.name)
-        y=ser.readline()
-# 	y=ser.read(ser.inWaiting())
-# 	time.sleep(0.01)
-# #        print x
-# 	x = y.replace("U", "")
-# 	print x
-# 	time.sleep(1)
+        # y=ser.readline()
+	# y=ser.read(ser.inWaiting())
+	# time.sleep(0.01)
+        #         print x
+	# x = y.replace("U", "")
+	# print x
+	# time.sleep(1)
         
-# 	if x == '3000E2000019060C01360510E09F488D':
-#          print "A"
-# 	 time.sleep(3)
+	# if x == '3000E2000019060C01360510E09F488D':
+        #  print "A"
+	#  time.sleep(3)
        
-#         elif x== '3000E2000019060C00770650D633EB30':
-#          print "B"
-# 	 time.sleep(3)
-#         else:
-#          print "none"
+        # elif x== '3000E2000019060C00770650D633EB30':
+        #  print "B"
+	#  time.sleep(3)
+        # else:
+        #  print "none"
 
